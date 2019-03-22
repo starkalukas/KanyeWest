23afd23078b6e7fdaa43197d1e067fa1f968f512
StarkaLukas <43465679+StarkaLukas@users.noreply.github.com>
2019-03-22 10:08:18 +0100

    Add files via upload

---
+++ javascript/showhide.js
@@ -0,0 +1,13 @@
+//JavaScript Document
+
+$('#hide').click(function(){
+    $('#good_morning').hide();
+    $('#hide').hide();
+    $('#show').show();
+});
+
+$('#show').click(function(){
+    $('#good_morning').show();
+    $('#hide').show();
+    $('#show').hide();
+});
